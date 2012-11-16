bc4d49d7252b8f6a261a3857c4299b7f4113e98d
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-11-16 23:31:21 +0100

    Add little utility to merge an object into another.

---
+++ utils.js
@@ -0,0 +1,13 @@
+
+exports.merge = function( a, b )
+{
+    for( var key in b )
+    {
+        if( b.hasOwnProperty( key ) )
+        {
+            a[ key ] = b[ key ];
+        }
+    }
+
+    return a;
+};
